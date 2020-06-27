94404da2f6cdb2355c83b3e6a852ff0ce229b119
shahzaib sohail <shahzaibsohail0077@gmail.com>
2020-06-27 22:46:46 +0500

    Ass5-Rocket-Done

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn add() -> &'static' str {
+    "hi, Please enter the any number in url as like (/10) to add-it "
+}
+
+#[get("/<num>")]
+fn num(num:u32) -> String{
+    format!("Number is:  {} after addition of:  {}",num,num+10)
+}
+
+
+fn main() {
+    rocket::ignite().mount("/", routes![add,num]).launch();
+}
